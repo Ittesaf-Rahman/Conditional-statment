fa1b731b72e54061f4b1221b7b3d473d4f0e30af
Ittesaf-Rahman <ittesafrahman2011@gmail.com>
2024-08-28 22:37:02 +0600

    Update Conditional Statements.py

--- Conditional Statements.py
+++ Conditional Statements.py
@@ -3,3 +3,11 @@ if num > 0:
     print("Your number is positive")
 else:
     print("Your number is nagenive")
+
+ac = float(input("please enter the actual product price  = "))
+sm = float(input("Enter your sales amount = "))
+if ac < sm:
+    amount = sm - ac
+    print("You profit ",amount ,"taka")
+else:
+    print("You got no profit !!!")
