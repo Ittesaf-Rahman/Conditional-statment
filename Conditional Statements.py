5eb1dd7247b870fb20e72deec156dffcaec4e1a2
Ittesaf-Rahman <ittesafrahman2011@gmail.com>
2024-08-28 22:45:23 +0600

    Update Conditional Statements.py

--- Conditional Statements.py
+++ Conditional Statements.py
@@ -11,3 +11,15 @@ if ac < sm:
     print("You profit ",amount ,"taka")
 else:
     print("You got no profit !!!")
+
+num15 = int(input("Enter a number = "))
+if num15 > 15:
+    print("Your number", num15," is greater than 15")
+else:
+    print("Your number", num15 ," is leas than 15")
+
+number = int(input("Enter a number = "))
+if number%2==0:
+    print("your number is even")
+else:
+    print("Your number is odd")
